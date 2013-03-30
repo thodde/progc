33467959e2daa7d86956d80623019b21051d38d9
azafft <azafft@enernoc.com>
2013-03-30 13:57:09 -0400

    physical layer work

--- phy_layer.c
+++ phy_layer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "common.h"
 
 /*
@@ -7,7 +8,60 @@ bool sendFrame(Frame *outFrame) {
 };
  */
 
+int runClient(int port_dll) {
+    printf("I'm a client, my dll is at port: %i\n", port_dll);
+
+    while (1) {
+        break;
+    }
+
+    return 0;
+};
+
+int runServer(int port_dll, int port_external) {
+    printf("I'm a server, my dll is at port: %i and my server is listening on port: %i\n", port_dll, port_external);
+
+    while (1) {
+        break;
+
+    }
+    return 0;
+};
 
 int main(int argc, char *argv[]) {
-    printf("hello world, this is the physical layer");
+    if (argc < 2) {
+        printf("please initialize the physical layer using the following\n");
+        printf("\tphy_layer server listening_port dll_port\n");
+        printf("\tphy_layer client dll_port\n");
+        return(-1);
+    }
+    
+    if (strcmp(argv[1], "server") == 0) {
+        //server input validation
+        if (argc < 4) {
+            printf("\tphy_layer server listening_port dll_port\n");
+            return(-1);
+        }
+
+        int port_server = atoi(argv[2]);
+        int port_dll = atoi(argv[3]);
+
+        return runServer(port_dll, port_server);
+    }
+    else if (strcmp(argv[1], "client") == 0) {
+        //client input validation
+        if (argc < 3) {
+            printf("\tphy_layer client dll_port\n");
+            return(-1);
+        }
+
+        int port_dll = atoi(argv[2]);
+
+        return runClient(port_dll);
+    }
+    else {
+        printf("Unrecognized physical layer type.  Please use server or client");
+        return(-1);;
+    }
+    
 };
