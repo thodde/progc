be47ca37cc082b94333d7ab22edcf8f01801dcd5
azafft <azafft@enernoc.com>
2013-03-30 15:08:05 -0400

    physical layer work

--- common.h
+++ common.h
@@ -2,18 +2,28 @@
 
 #define     MAX_FRAME_SIZE          192
 #define     MAX_PACKET_SIZE         100
+#define     MESSAGE_INITIATOR       1
+#define     MESSAGE_TERMINATOR      23
 
-struct Message {
-    int type;
+typedef enum { Message_Join, Message_Kick, Message_Whisper } Message_Type;
+typedef enum { Frame_Init, Frame_Data, Frame_Ack, Frame_Final } Frame_Type;
+typedef enum { Packet_Init, Packet_Data, Packet_Ack, Packet_Final } Packet_Kind;
+
+
+typedef struct {
+    Message_Type type;
     char *data;
-};
+} Message;
 
-struct Frame {
-    int type;
-    int data[MAX_FRAME_SIZE];
-};
+typedef struct {
+    Frame_Type type;
+    int payload[MAX_FRAME_SIZE];
+} Frame;
 
-struct Packet {
-    int type;
-    int data[MAX_PACKET_SIZE];
-};
+typedef struct {
+    Packet_Kind type;
+    int payload[MAX_PACKET_SIZE];
+} Packet;
+
+
+
--- phy_layer.c
+++ phy_layer.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 #include "common.h"
 
 /*
@@ -8,60 +12,28 @@ bool sendFrame(Frame *outFrame) {
 };
  */
 
-int runClient(int port_dll) {
-    printf("I'm a client, my dll is at port: %i\n", port_dll);
+
+int run(int port_dll, int port_listening) {
+    printf("my dll is at port: %i and I'm listening on port: %i\n", port_dll, port_listening);
+
+    int listen_sd = socket(AF_INET, SOCK_STREAM, 0);
 
     while (1) {
         break;
     }
 
     return 0;
-};
-
-int runServer(int port_dll, int port_external) {
-    printf("I'm a server, my dll is at port: %i and my server is listening on port: %i\n", port_dll, port_external);
-
-    while (1) {
-        break;
-
-    }
-    return 0;
-};
+}
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("please initialize the physical layer using the following\n");
-        printf("\tphy_layer server listening_port dll_port\n");
-        printf("\tphy_layer client dll_port\n");
+    if (argc < 3) {
+        printf("please initialize the physical layer using the following:\n");
+        printf("\tphy_layer listening_port dll_port\n");
         return(-1);
     }
-    
-    if (strcmp(argv[1], "server") == 0) {
-        //server input validation
-        if (argc < 4) {
-            printf("\tphy_layer server listening_port dll_port\n");
-            return(-1);
-        }
 
-        int port_server = atoi(argv[2]);
-        int port_dll = atoi(argv[3]);
+    int port_server = atoi(argv[2]);
+    int port_dll = atoi(argv[3]);
 
-        return runServer(port_dll, port_server);
-    }
-    else if (strcmp(argv[1], "client") == 0) {
-        //client input validation
-        if (argc < 3) {
-            printf("\tphy_layer client dll_port\n");
-            return(-1);
-        }
-
-        int port_dll = atoi(argv[2]);
-
-        return runClient(port_dll);
-    }
-    else {
-        printf("Unrecognized physical layer type.  Please use server or client");
-        return(-1);;
-    }
-    
+    return run(port_dll, port_server);
 };
